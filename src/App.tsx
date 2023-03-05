c43e7d44cfa5bc07b0ef4180df22f452d468b7b6
KrinjMaster <113294830+KrinjMaster@users.noreply.github.com>
2023-03-05 22:52:07 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="app">
+      <h1 className="font-bold text-center mt-11 text-white text-[100px]">Template is installed!</h1>
+    </div>
+  )
+}
+
+export default App
