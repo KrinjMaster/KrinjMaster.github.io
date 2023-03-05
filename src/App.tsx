ecbb77e227678e234e369c98403dc5498e0ccb2b
KrinjMaster <5opkasuka@gmail.com>
2023-03-05 23:53:41 +0300

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 function App() {
   return (
     <div className="app">
-      <h1 className="font-bold text-center mt-11 text-white text-[100px]">Template is installed!</h1>
+      <div className="container w-screen snap-y h-screen snap-mandatory overflow-y-auto">
+        <section className="h-[100vh] bg-zinc-800 w-screen snap-start">
+          <h1 className="font-bold text-center text-white text-[100px]">Main Section</h1>
+        </section>
+        <section className="h-[100vh] bg-zinc-600 w-screen snap-start">
+          <h1 className="font-bold text-center text-white text-[100px]">Second Section</h1>
+        </section>
+        <section className="h-[100vh] bg-zinc-400 w-screen snap-start">
+          <h1 className="font-bold text-center text-white text-[100px]">Third Section</h1>
+        </section>
+      </div>
     </div>
   )
 }
